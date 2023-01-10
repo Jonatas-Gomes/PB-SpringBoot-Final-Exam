e3cd26c039bd7d6c6928031842437a49d9021b60
Jônatas <jonatas.bezerra.pb@compasso.com.br>
2023-01-10 12:20:21 -0300

    Adicionando as pastas iniciais dos microserviços

---
+++ Ms-History/src/test/java/br/com/compass/pb/MsHistory/MsHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.pb.MsHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
