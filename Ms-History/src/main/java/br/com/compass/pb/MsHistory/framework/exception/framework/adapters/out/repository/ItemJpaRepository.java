149c19cc80c42a83c92a4182a976f33be5dcf176
Jônatas <jonatas.bezerra.pb@compasso.com.br>
2023-01-16 17:02:42 -0300

    Revert "Refactor endpoint get"
    This reverts commit cb4962010297ca9ab834d400f154d6d941dd2746.

--- Ms-History/src/main/java/br/com/compass/pb/MsHistory/framework/exception/framework/adapters/out/repository/ItemJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.compass.pb.MsHistory.framework.exception.framework.adapters.out.repository;
-
-import br.com.compass.pb.msorder.domain.model.Item;
-import br.com.compass.pb.msorder.domain.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemJpaRepository extends JpaRepository<Item, Long> {
-    <S extends Order> S save(S entity);
-}
