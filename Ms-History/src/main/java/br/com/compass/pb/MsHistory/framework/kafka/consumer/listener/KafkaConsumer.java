5c7b761f86f7b8f04d3f3a52944f2201ae0790a6
Jônatas <jonatas.bezerra.pb@compasso.com.br>
2023-01-20 02:47:41 -0300

    Alterações no Kafka Consumer

--- Ms-History/src/main/java/br/com/compass/pb/MsHistory/framework/kafka/consumer/listener/KafkaConsumer.java
+++ Ms-History/src/main/java/br/com/compass/pb/MsHistory/framework/kafka/consumer/listener/KafkaConsumer.java
@@ -3,14 +3,17 @@ package br.com.compass.pb.MsHistory.framework.kafka.consumer.listener;
 import br.com.compass.pb.MsHistory.application.port.out.HistoryPortOut;
 import br.com.compass.pb.MsHistory.domain.dto.MessageOrderDTO;
 import br.com.compass.pb.MsHistory.domain.model.History;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.TimeZone;
@@ -20,10 +23,14 @@ import java.util.TimeZone;
 @RequiredArgsConstructor
 public class KafkaConsumer {
     private final HistoryPortOut portOut;
-    @KafkaListener(topics = "$(topic.order-topic)", groupId = "group_id")
-    public void consumeOrder(ConsumerRecord<String, String> payload) throws IOException{
+    @KafkaListener(topics = "${topic.order-topic}", groupId = "group_id")
+    public void consumeOrder(ConsumerRecord<String, String> payload) throws JsonProcessingException {
         log.info("message consumed");
+
         ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+
         var messageOrderDTO = mapper.readValue(payload.value(), MessageOrderDTO.class);
         saveMessageOrder(payload.timestamp(), messageOrderDTO);
         log.info("order message saved!");
@@ -33,7 +40,7 @@ public class KafkaConsumer {
         LocalDateTime time = LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), TimeZone.getDefault().toZoneId());
 
         var history = History.builder()
-                .idOrder(messageOrderDTO.getIdOrder())
+                .idOrder(messageOrderDTO.getOrderId())
                 .total(messageOrderDTO.getTotal())
                 .eventDate(time)
                 .build();
